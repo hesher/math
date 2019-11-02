ca32761c11dd01babebf6ddac9bbd8d96525e02d
Ben Katz <bkatz@fb.com>
2019-11-02 17:43:04 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function useSet(initial = []) {
+  const [selected, setSelected] = useState(initial);
+  const theSet = new Set(selected);
+
+  return [
+    selected,
+    x => {
+      theSet.add(x);
+      setSelected(Array.from(theSet.values()));
+    },
+    x => {
+      theSet.delete(x);
+      setSelected(Array.from(theSet.values()));
+    },
+    setSelected
+  ];
+}
 
 function App() {
+  const [selected, add, remove, setSelected] = useSet([]);
+  const [sum, setSum] = useState();
+  const [money, setMoney] = useState(0);
+  const [prize, setPrize] = useState(0);
+  const [totalPrize, setTotalPrize] = useState(0);
+  const [success, setSuccess] = useState(false);
+  const [failed, setFailed] = useState(false);
+
+  const reset = () => {
+    const theSelected = [1, 3, 5, 7, 11, 13, 16];
+    setSelected(theSelected);
+    setSum(theSelected.reduce((x, y) => x + (Math.random() > 0.5 ? y : 0), 0));
+    setPrize(Math.ceil(Math.random() * 5));
+    setSuccess(false);
+    setFailed(false);
+  };
+
+  useEffect(() => {
+    reset();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {success ? (
+        <Success />
+      ) : failed ? (
+        <Failure />
+      ) : (
+        <span style={{ display: "flex" }}>
+          <span className="stats">
+            <div>Sum = {sum}</div>
+            <div>Prize= ${prize}</div>
+            <div>Money = ${money}</div>
+          </span>
+          <header className="container">
+            {selected.map(num => (
+              <button
+                className="num"
+                onClick={() => {
+                  remove(num);
+                  setSum(sum - num);
+                  if (sum - num === 0) {
+                    setMoney(money + prize);
+                    setTotalPrize(prize + totalPrize);
+                    setSuccess(true);
+                    setTimeout(() => {
+                      reset();
+                    }, 1000);
+                  } else if (sum - num < 0 || cantSum(selected, sum)) {
+                    setMoney(money - prize);
+                    setFailed(true);
+                    setTimeout(() => {
+                      reset();
+                    }, 1000);
+                  }
+                }}
+              >
+                {num}
+              </button>
+            ))}
+          </header>
+        </span>
+      )}
     </div>
+  );
+}
+
+const Success = () => <div style={{ fontSize: 36 }}> SUCCESS!!! :) :)</div>;
+const Failure = () => <div style={{ fontSize: 36 }}>Failed!!! :( :( </div>;
+
+function cantSum(selected, sum) {
+  return (
+    selected.reduce((x, y) => x + y, 0) < sum || selected.every(x => x > sum)
   );
 }
 
