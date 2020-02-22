20b067f28593554c3a1a6877667585de8ee763bc
Ben Katz <ben.katz@gmail.com>
2020-02-22 21:50:24 +0000

    Updates

---
+++ precache-manifest.fdbea60087daa1de5bb0e03c063a9f48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f1bedd01ee642314d2d49e3387fd41",
+    "url": "/math/index.html"
+  },
+  {
+    "revision": "02cf2634e133c6e16c23",
+    "url": "/math/static/css/main.5f003258.chunk.css"
+  },
+  {
+    "revision": "04cc94e54283ba9eb1ac",
+    "url": "/math/static/js/2.7d868268.chunk.js"
+  },
+  {
+    "revision": "add6e1e24932b45e5cae968f40b0242f",
+    "url": "/math/static/js/2.7d868268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02cf2634e133c6e16c23",
+    "url": "/math/static/js/main.7d34da98.chunk.js"
+  },
+  {
+    "revision": "f747be10c04f40ac493f",
+    "url": "/math/static/js/runtime-main.28eea1aa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/math/static/media/logo.25bf045c.svg"
+  }
+]);
