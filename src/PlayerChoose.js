b2230bc774f8f642362d635c65ae8d35e5a94c58
Ben Katz <ben.katz@gmail.com>
2020-02-23 18:29:06 +0000

    design and new plauer chooser

---
+++ src/PlayerChoose.js
@@ -0,0 +1,11 @@
+// import React, {useState, useEffect} from 'react';
+// import AddToSum from './AddToSum';
+// import './App.css';
+// import RESULT from './RESULT';
+// import selectRandomN from './selectRandomN';
+// import * as firebase from 'firebase/app';
+// import 'firebase/database';
+
+// var dbRef = firebase.database().ref('/users/zohar');
+
+// export default function PlayerChoose() {}
